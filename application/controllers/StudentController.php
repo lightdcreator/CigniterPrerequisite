d03336b46680a5c1bc33814f275ac5e08f868cb3
hilomafrancisoliver <hilomafrancisoliver@gmail.com>
2024-02-24 16:42:44 +0800

    Passing id parameter to model and execute it to show

--- application/controllers/StudentController.php
+++ application/controllers/StudentController.php
@@ -5,11 +5,29 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class StudentController extends CI_Controller
 {
 
-
+	//Other ways to load data assign value and store
 	public function studentvar()
 	{
-		$this->load->model('student/Student_model');
-		$student = $this->Student_model->student_data();
+		//shorcut naming for model
+		$this->load->model('student/Student_model', 'stud');
+		//$student = $this->Student_model->student_data();
+
+		// $student = new Student_model;
+		// $student = $student->student_data();
+
+		$student = $this->stud->student_data();
+
 		echo "Student Name : "  . $student;
+	}
+
+
+
+	//Acessing the parameter
+	public function execute($id)
+	{
+
+		$this->load->model('student/Student_model', 'stud');
+		$select_user = $this->stud->execute_user($id);
+		echo $select_user;
 	}
 }
--- application/models/student/Student_model.php
+++ application/models/student/Student_model.php
@@ -5,8 +5,22 @@ class Student_model extends CI_Model
 {
 	public function student_data()
 	{
+		$student_course = $this->students_course();
+		return $stud_name = "Mark" .  " Course :" . $student_course;
+	}
 
-		return $stud_name = "Mark";
+	private function students_course()
+	{
+		return $student_course = "CSS";
+	}
+
+	public function execute_user($id)
+	{
+		if ($id == '1') {
+			return $result = "User 1";
+		} elseif ($id == '2') {
+			return $result = "User 2";
+		}
 	}
 }
 
